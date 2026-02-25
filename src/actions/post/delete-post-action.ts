eb42945c1e1afe728a5961f27136aa947e214d96
Ruan C4rlos <crrc1915@gmail.com>
2026-02-24 23:09:58 -0300

    feat(admin): estilizar lista, mover DELETE para client, adicionar server actions e dialog

---
+++ src/actions/post/delete-post-action.ts
@@ -0,0 +1,9 @@
+"use server";
+
+import { asyncDelay } from "@/src/utils/async-delay";
+import { logColor } from "@/src/utils/log-color";
+export async function deletePostAction(id: string) {
+  await asyncDelay(2000);
+  logColor("Deleting post with ID: " + id);
+  return id;
+}
--- src/components/PostsListAdmin/index.tsx
+++ src/components/PostsListAdmin/index.tsx
@@ -1,12 +1,33 @@
 import { findAllPostAdmin } from "@/src/lib/post/queries/admin";
+import clsx from "clsx";
+import Link from "next/link";
+import { DeletePostButton } from "../admin/DeletePostButton";
 
 export default async function PostsListAdmin() {
   const posts = await findAllPostAdmin();
 
   return (
-    <div className="py-16">
+    <div className="mb-16">
       {posts.map((post) => {
-        return <p key={post.id}>{post.title}</p>;
+        return (
+          <div
+            className={clsx(
+              "py-2 px-2",
+              !post.published && "bg-slate-300",
+              "flex gap-2 items-center justify-between",
+            )}
+            key={post.id}
+          >
+            <Link href={`/admin/post/${post.id}`}>{post.title}</Link>
+
+            {!post.published && (
+              <span className="text-xs text-slate-600 italic">
+                (Não publicado)
+              </span>
+            )}
+            <DeletePostButton id={post.id} title={post.title} />
+          </div>
+        );
       })}
     </div>
   );
