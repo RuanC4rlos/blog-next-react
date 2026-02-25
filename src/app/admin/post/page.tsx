6bacf187c5f889f6c7573b9369fa716cec616bc8
Ruan C4rlos <crrc1915@gmail.com>
2026-02-24 21:14:49 -0300

    feat: Criar rotas da área administrativa do site (admin)

---
+++ src/app/admin/post/page.tsx
@@ -0,0 +1,9 @@
+export const dynamic = "force-dynamic";
+
+export default async function AdminPostPage() {
+  return (
+    <div className="py-16 text-6xl">
+      <h1>Admin Post Page</h1>
+    </div>
+  );
+}
