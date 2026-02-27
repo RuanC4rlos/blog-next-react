edd7e733a718f212608217f15f1568934480aee3
Ruan C4rlos <crrc1915@gmail.com>
2026-02-26 21:24:41 -0300

    feat(ui): criar componentes InputText e InputCheckbox

--- src/app/admin/post/new/page.tsx
+++ src/app/admin/post/new/page.tsx
@@ -1,46 +1,49 @@
 import { Button } from "@/src/components/Button";
-import { BanIcon, BugIcon, CheckIcon } from "lucide-react";
+import { InputCheckbox } from "@/src/components/InputCheckbox";
+import { InputText } from "@/src/components/InputText";
 
 export const dynamic = "force-dynamic";
 
 export default async function AdminPostNewPage() {
   return (
-    <div>
-      <div className="py-16 flex gap-4 flex-wrap items-center">
-        <Button variant="default" size="sm">
-          <BugIcon /> Confirma
-        </Button>
-        <Button variant="ghost" size="md">
-          <BugIcon /> Confirma
-        </Button>
-        <Button variant="danger" size="lg">
-          <BugIcon /> Confirma
-        </Button>
+    <form action="" className="mb-16">
+      <div className="flex flex-col gap-6">
+        <InputText
+          labelText="Nome"
+          placeholder="Digite seu nome"
+          type="password"
+        />
+        <InputText labelText="Sobrenome" placeholder="Digite seu sobrenome" />
+
+        <InputCheckbox labelText="Sobrenome" />
+
+        <InputText
+          disabled
+          labelText="Sobrenome"
+          placeholder="Digite seu sobrenome"
+          defaultValue="Olá mundo"
+        />
+        <InputText
+          disabled
+          labelText="Sobrenome"
+          placeholder="Digite seu sobrenome"
+        />
+        <InputText
+          labelText="Sobrenome"
+          placeholder="Digite seu sobrenome"
+          readOnly
+        />
+        <InputText
+          labelText="Sobrenome"
+          placeholder="Digite seu sobrenome"
+          defaultValue="Olá mundo"
+          readOnly
+        />
+
+        <div className="mt-4">
+          <Button type="submit">Enviar</Button>
+        </div>
       </div>
-
-      <div className="py-16 flex gap-4 flex-wrap items-center">
-        <Button variant="default" size="sm" disabled>
-          <BugIcon /> Confirma
-        </Button>
-        <Button variant="ghost" size="md" disabled>
-          <BugIcon /> Confirma
-        </Button>
-        <Button variant="danger" size="lg" disabled>
-          <BugIcon /> Confirma
-        </Button>
-
-        <Button variant="danger" size="lg" className="w-full">
-          <BugIcon /> Confirma
-        </Button>
-
-        <Button variant="ghost" size="lg" className="w-full">
-          <BanIcon /> Cancel
-        </Button>
-
-        <Button variant="default" size="lg" className="w-full">
-          <CheckIcon /> OK
-        </Button>
-      </div>
-    </div>
+    </form>
   );
 }
