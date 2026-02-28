84934465b31029bf34c82de34d1e7ba6640b6d9d
Ruan C4rlos <crrc1915@gmail.com>
2026-02-27 21:54:08 -0300

    feat(posts): cria formulário de post com integração do editor Markdown

---
+++ src/components/admin/ManagePostForm/index.tsx
@@ -0,0 +1,59 @@
+"use client";
+
+import { useState } from "react";
+import { Button } from "../../Button";
+import { InputCheckbox } from "../../InputCheckbox";
+import { InputText } from "../../InputText";
+import { MarkdownEditor } from "../../MarkdownEditor";
+
+export function ManagePostForm() {
+  const [contentValue, setContentValue] = useState("");
+  return (
+    <form action="" className="mb-16">
+      <div className="flex flex-col gap-6">
+        <InputText
+          labelText="Nome"
+          placeholder="Digite seu nome"
+          type="password"
+        />
+        <MarkdownEditor
+          labelText="Conteúdo"
+          disabled={false}
+          textAreaName="content"
+          value={contentValue}
+          setValue={setContentValue}
+        />
+        <InputText labelText="Sobrenome" placeholder="Digite seu sobrenome" />
+
+        <InputCheckbox labelText="Sobrenome" />
+
+        <InputText
+          disabled
+          labelText="Sobrenome"
+          placeholder="Digite seu sobrenome"
+          defaultValue="Olá mundo"
+        />
+        <InputText
+          disabled
+          labelText="Sobrenome"
+          placeholder="Digite seu sobrenome"
+        />
+        <InputText
+          labelText="Sobrenome"
+          placeholder="Digite seu sobrenome"
+          readOnly
+        />
+        <InputText
+          labelText="Sobrenome"
+          placeholder="Digite seu sobrenome"
+          defaultValue="Olá mundo"
+          readOnly
+        />
+
+        <div className="mt-4">
+          <Button type="submit">Enviar</Button>
+        </div>
+      </div>
+    </form>
+  );
+}
