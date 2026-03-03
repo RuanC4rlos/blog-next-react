12d0ce5f531533b8e18a811e602b4b463d3aef7d
Ruan C4rlos <crrc1915@gmail.com>
2026-03-02 22:30:53 -0300

    feat: Criar métodos create, delete e update no repositório

--- src/actions/post/create-post-action.ts
+++ src/actions/post/create-post-action.ts
@@ -1,10 +1,9 @@
 "use server";
 
-import { drizzleDb } from "@/src/db/drizzle";
-import { postsTable } from "@/src/db/drizzle/schemas";
 import { makePartialPublicPost, PublicPost } from "@/src/dto/post/dto";
 import { PostCreateSchema } from "@/src/lib/post/validations";
 import { PostModel } from "@/src/models/post/post-model";
+import { postRepository } from "@/src/repositories/post";
 import { makeSlugFromText } from "@/src/utils/make-slug-from-text";
 import { revalidateTag } from "next/cache";
 import { redirect } from "next/navigation";
@@ -47,8 +46,21 @@ export async function createPostAction(
     slug: makeSlugFromText(validPostData.title),
   };
 
-  await drizzleDb.insert(postsTable).values(newPost);
+  try {
+    await postRepository.create(newPost);
+  } catch (e: unknown) {
+    if (e instanceof Error) {
+      return {
+        formState: newPost,
+        errors: [e.message],
+      };
+    }
 
+    return {
+      formState: newPost,
+      errors: ["Erro desconhecido"],
+    };
+  }
   revalidateTag("posts", "max");
   redirect(`/admin/post/${newPost.id}`);
 }
--- src/actions/post/delete-post-action.ts
+++ src/actions/post/delete-post-action.ts
@@ -1,32 +1,29 @@
 "use server";
 
-import { drizzleDb } from "@/src/db/drizzle";
-import { postsTable } from "@/src/db/drizzle/schemas";
 import { postRepository } from "@/src/repositories/post";
-import { asyncDelay } from "@/src/utils/async-delay";
-import { logColor } from "@/src/utils/log-color";
-import { eq } from "drizzle-orm";
 import { revalidateTag } from "next/cache";
 
 export async function deletePostAction(id: string) {
-  await asyncDelay(2000);
-  logColor("Deleting post with ID: " + id);
-
   if (!id || typeof id !== "string") {
     return {
       error: "Dados inválidos",
     };
   }
+  let post;
+  try {
+    post = await postRepository.delete(id);
+  } catch (e: unknown) {
+    if (e instanceof Error) {
+      return {
+        error: e.message,
+      };
+    }
 
-  const post = await postRepository.findById(id).catch(() => undefined);
-
-  if (!post) {
     return {
-      error: "Post não encontrado",
+      error: "Erro desconhecido",
     };
   }
 
-  await drizzleDb.delete(postsTable).where(eq(postsTable.id, id));
   revalidateTag("posts", "max");
   revalidateTag(`post-${post.slug}`, "max");
 
